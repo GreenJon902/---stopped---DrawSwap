af35acf3e268dd807e5535379f1b0883436b800e
jj b <jj@thebabers.at>
2021-02-03 10:44:51 +0000

    added base setup and kivy setup

---
+++ AppInfo.py
@@ -0,0 +1,28 @@
+import os
+import appdirs
+
+appname = "DrawSwap"
+appauthor = "GreenJon902"
+version = "Beta-V0.0.1-AA00"
+roaming = False
+
+array = {"appname": "DrawSwap",
+         "appauthor": "GreenJon902",
+         "version": "Beta-V0.0.1-AA00",
+         "roaming": False}
+
+AppDirs: appdirs.AppDirs = appdirs.AppDirs(**array)
+
+user_data_dir: str = AppDirs.user_data_dir
+kivy_home_dir: str = os.path.join(user_data_dir, "kivy")
+config_dir: str = os.path.join(user_data_dir, "config")
+code_dir: str = os.path.dirname(os.path.realpath(__file__))
+default_user_data_file: str = os.path.join(code_dir, "default_user_data.json")
+user_data_file: str = os.path.join(user_data_dir, "user_data.json")
+
+log_dir: str = AppDirs.user_log_dir
+
+__all__ = ["appname", "appauthor", "version", "roaming",
+           "array",
+           "user_data_dir", "kivy_home_dir", "config_dir", "code_dir", "default_user_data_file", "user_data_file",
+           "log_dir"]
---
+++ DrawSwap.py
@@ -0,0 +1,22 @@
+import os
+import pathlib
+from multiprocessing import freeze_support
+from shutil import copyfile
+
+import AppInfo
+
+if __name__ == "__main__":
+    freeze_support()
+
+    if not os.path.exists(AppInfo.user_data_dir):
+        os.makedirs(AppInfo.user_data_dir)
+
+    if not os.path.exists(AppInfo.user_data_file):
+        copyfile(AppInfo.default_user_data_file, AppInfo.user_data_file)
+
+    os.chdir(pathlib.Path(__file__).parent.absolute())
+    os.environ["KIVY_NO_ARGS"] = "1"
+    os.environ["KIVY_HOME"] = AppInfo.kivy_home_dir
+    os.environ["KCFG_KIVY_LOG_NAME"] = "%y-%m-%d_%_.log"
+    os.environ["KCFG_KIVY_LOG_DIR"] = AppInfo.log_dir
+    os.environ["KCFG_KIVY_LOG_LEVEL"] = "info"
