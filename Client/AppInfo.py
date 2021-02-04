c6cda575f685bd903a47507c04db5e0f7ad1a1e2
jj b <jj@thebabers.at>
2021-02-04 10:21:50 +0000

    added texture link

--- Client/AppInfo.py
+++ Client/AppInfo.py
@@ -19,10 +19,12 @@ config_dir: str = os.path.join(user_data_dir, "config")
 code_dir: str = os.path.dirname(os.path.realpath(__file__))
 default_user_data_file: str = os.path.join(code_dir, "default_user_data.json")
 user_data_file: str = os.path.join(user_data_dir, "user_data.json")
+resources_dir: str = os.path.join(code_dir, "Resources")
+texture_link_file: str = os.path.join(resources_dir, "textureLink.ini")
 
 log_dir: str = AppDirs.user_log_dir
 
 __all__ = ["appname", "appauthor", "version", "roaming",
            "array",
            "user_data_dir", "kivy_home_dir", "config_dir", "code_dir", "default_user_data_file", "user_data_file",
-           "log_dir"]
+           "log_dir", "resources_dir", "texture_link_file"]
---
+++ Client/staticConfigurables.py
@@ -0,0 +1,6 @@
+from configparser import ConfigParser
+
+import AppInfo
+
+textures = ConfigParser()
+textures.read(AppInfo.textureLink_file)
