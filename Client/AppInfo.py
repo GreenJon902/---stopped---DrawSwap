bf1db713a2ba540c072224030da725ade6655922
jj b <jj@thebabers.at>
2021-02-04 10:29:49 +0000

    got loading in and start of kivy

--- Client/AppInfo.py
+++ Client/AppInfo.py
@@ -21,10 +21,11 @@ default_user_data_file: str = os.path.join(code_dir, "default_user_data.json")
 user_data_file: str = os.path.join(user_data_dir, "user_data.json")
 resources_dir: str = os.path.join(code_dir, "Resources")
 texture_link_file: str = os.path.join(resources_dir, "textureLink.ini")
+kv_language_dir: str = os.path.join(resources_dir, "kv_language")
 
 log_dir: str = AppDirs.user_log_dir
 
 __all__ = ["appname", "appauthor", "version", "roaming",
            "array",
            "user_data_dir", "kivy_home_dir", "config_dir", "code_dir", "default_user_data_file", "user_data_file",
-           "log_dir", "resources_dir", "texture_link_file"]
+           "log_dir", "resources_dir", "texture_link_file", "kv_language_dir"]
---
+++ Client/Graphics/__init__.py
@@ -0,0 +1,1 @@
+from drawSwap import *
