ab6531f3187614546b6564f454dc524f9c807222
jj b <jj@thebabers.at>
2021-02-04 14:31:45 +0000

    added test to see img sizes by canvas on splacsh screen

--- Graphics/Screens/splashScreen.py
+++ Graphics/Screens/splashScreen.py
@@ -1,6 +1,26 @@
+import random
+
+from kivy.graphics.context_instructions import Color
+from kivy.graphics.vertex_instructions import Rectangle
+from kivy.uix.image import Image
 from kivy.uix.screenmanager import Screen
 
 
 class SplashScreen(Screen):
-    def on_pre_enter(self, *args, **kwargs):
-        super(SplashScreen, self).on_pre_enter(*args, **kwargs)
+    def on_size(self, *args):
+        def walk(widget):
+            for child in widget.children:
+                walk(child)
+
+            if widget.__class__ == Image:
+                with widget.canvas:
+                    widget.canvas.clear()
+
+                    col = random.random(), random.random(), random.random()
+                    Color(rgb=col)
+                    print(col, widget.pos, widget.size)
+
+                    Rectangle(pos=widget.pos, size=widget.size)
+
+        for child in self.children:
+            walk(child)
