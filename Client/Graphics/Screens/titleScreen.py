0c62c2f21481c9c839134ac98075c7f481a79d7b
jj b <jj@thebabers.at>
2021-02-26 10:28:52 +0000

    added lots of conectivity and stuff to the config made play screen convert work

--- Client/Graphics/Screens/titleScreen.py
+++ Client/Graphics/Screens/titleScreen.py
@@ -1,5 +1,14 @@
+from kivy.logger import Logger
 from kivy.uix.screenmanager import Screen
 
 
 class TitleScreen(Screen):
-    pass
+    content_image = None
+
+    def play_button_pressed(self):
+        self.content_image = self.ids["content"].export_as_image().texture
+        self.content_image.uvpos = (0, 1)
+        self.content_image.uvsize = (1, -1)
+        Logger.info("TitleScreen: Content exported as image for play screen")
+
+        self.parent.set_screen("PlayScreen")
--- Client/staticConfigurables.py
+++ Client/staticConfigurables.py
@@ -1,5 +1,5 @@
 import os
-from configparser import ConfigParser
+from configparser import ConfigParser, ExtendedInterpolation
 
 import AppInfo
 
@@ -10,14 +10,14 @@ class _pathConfigParser(ConfigParser):
         return os.path.join(AppInfo.resources_dir, path)
 
 
-textures = _pathConfigParser()
+textures = _pathConfigParser(interpolation=ExtendedInterpolation())
 textures.read(AppInfo.texture_link_file)
 
-fonts = _pathConfigParser()
+fonts = _pathConfigParser(interpolation=ExtendedInterpolation())
 fonts.read(AppInfo.font_link_file)
 
-settings = ConfigParser(interpolation=None)
+settings = ConfigParser(interpolation=ExtendedInterpolation())
 settings.read(AppInfo.settings_file)
 
-graphics = ConfigParser(interpolation=None)
+graphics = ConfigParser(interpolation=ExtendedInterpolation())
 graphics.read(AppInfo.graphics_file)
