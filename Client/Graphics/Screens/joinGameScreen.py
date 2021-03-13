9eb2bd7616085216918c438e7b61cc02d93225ff
jj b <jj@thebabers.at>
2021-03-13 15:34:52 +0000

    fixed hoverbutton positioning

--- Client/Graphics/Screens/joinGameScreen.py
+++ Client/Graphics/Screens/joinGameScreen.py
@@ -13,7 +13,12 @@ class JoinGameScreen(Screen, ScreenWhichIncludesHoverButton):
         self.background_c_width = self.ids["background_c"].width
         a = Animation(background_c_width=0, duration=staticConfigurables.graphics.getfloat("JoinGameScreen",
                                                                                            "page_turn_speed"))
+        a.bind(on_complete=self.hide_background_c)
         a.start(self)
 
     def on_background_c_width(self, _, width):
         self.ids["background_c"].width = width
+
+    def hide_background_c(self, *args, **kwargs):
+        self.ids["background_c"].opacity = 0
+
