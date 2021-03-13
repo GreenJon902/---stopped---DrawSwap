b8727d06ae07191fb7e56888364d7159ff4bb3b0
jj b <jj@thebabers.at>
2021-03-13 17:47:03 +0000

    set up basic server

---
+++ Server/__main__.py
@@ -0,0 +1,7 @@
+from entrance import Entrance
+
+if __name__ == "__main__":
+    print("Starting DrawSwap server!")
+
+    e = Entrance()
+    e.start()
---
+++ Server/entrance.py
@@ -0,0 +1,25 @@
+import socket
+
+from config import config
+
+
+class Entrance:
+    def __init__(self):
+        self.port = config.getint("ServerSettings", "port")
+        self.s = None
+
+        print("Entrance is initiated")
+
+    def start(self):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        self.s.bind(("", self.port))
+        print("Socket bound to", self.port)
+
+        self.s.listen(5)
+        print("Socket is listening")
+
+    def accept_incoming_connections(self):
+        while True:
+            client, client_address = self.s.accept()
+            print(client, client_address)
