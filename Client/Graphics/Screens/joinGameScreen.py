502146a265e1b7fb25f427e6e39665777b4b671f
jj b <jj@thebabers.at>
2021-03-13 14:29:35 +0000

    removed debug print statments

--- Client/Graphics/Screens/joinGameScreen.py
+++ Client/Graphics/Screens/joinGameScreen.py
@@ -9,13 +9,10 @@ class JoinGameScreen(Screen):
     background_c_width = NumericProperty()
 
     def on_enter(self, *args):
-        print(self.ids["background_c"].pos)
-
         self.background_c_width = self.ids["background_c"].width
         a = Animation(background_c_width=0, duration=staticConfigurables.graphics.getfloat("JoinGameScreen",
                                                                                            "page_turn_speed"))
         a.start(self)
 
     def on_background_c_width(self, _, width):
-        print("test", width)
         self.ids["background_c"].width = width
